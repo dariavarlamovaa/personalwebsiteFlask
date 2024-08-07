739f1c31ca789944944cab2dc2831d2b76e36b4f
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-08-07 22:48:33 +0300

    updated media queries

---
+++ static/js/burger-menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger-menu').addEventListener('click', function() {
+  document.querySelector('.navigation').classList.toggle('active');
+  document.querySelector('.burger-menu').classList.toggle('active');
+});
