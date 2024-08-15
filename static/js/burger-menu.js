b291b848715be1cc9ec474a46c439f983ab22ff9
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2024-08-15 15:31:52 +0300

    updated media queries, added event to burger menu, changed wavy line image

--- static/js/burger-menu.js
+++ static/js/burger-menu.js
@@ -1,4 +1,27 @@
-document.querySelector('.burger-menu').addEventListener('click', function() {
-  document.querySelector('.navigation').classList.toggle('active');
-  document.querySelector('.burger-menu').classList.toggle('active');
+const burgerMenu = document.querySelector('.burger-menu')
+const navigation = document.querySelector('.navigation')
+let isMainMenuActive = false
+
+burgerMenu.addEventListener('click', function () {
+    if (!isMainMenuActive) {
+        navigation.classList.add('active');
+        burgerMenu.classList.add('active');
+    } else {
+        navigation.classList.remove('active');
+        burgerMenu.classList.remove('active');
+    }
+    isMainMenuActive = !isMainMenuActive
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('click', function (event) {
+        if (isMainMenuActive) {
+            let isClickInsideMenu = navigation.contains(event.target);
+            if (!isClickInsideMenu && event.target !== document.querySelector('.burger-menu')) {
+                navigation.classList.remove('active');
+                burgerMenu.classList.toggle('active');
+                isMainMenuActive = false;
+            }
+        }
+    });
 });
